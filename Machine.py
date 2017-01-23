5382d05dac31838d4b6dd29fd72ff74751f9b547
Zain Afzal <zain.afz@gmail.com>
2017-01-23 21:21:16 +1100

    making my branch match rsaber-flask the dodgy way (step 2)

---
+++ Machine.py
@@ -0,0 +1,76 @@
+import re
+
+class Machine():
+    def __init__(self, length, height):
+        self.memory = [0]*length*height
+        
+        self.size = length*height;
+        self.length = length
+        self.height = height
+
+        self.pc = 0
+        self.instruction = {
+            'a' : 0,
+            'b' : 0,
+            'c' : 0,
+        }
+
+    def fetch(self):
+        self.instruction['a'] = self.memory[self.pc]
+        self.instruction['b'] = self.memory[self.pc+1]
+        self.instruction['c'] = self.memory[self.pc+2]
+
+    def execute(self):
+        # subleq a, b, c   ; Mem[b] = Mem[b] - Mem[a]
+        # if (Mem[b] <= 0) goto c
+
+        if self.instruction['a'] < 0 or self.instruction['b'] < 0:
+            self.pc -= 1
+        else:
+            self.memory[self.instruction['b']] -= self.memory[self.instruction['a']]
+
+            if self.memory[self.instructuction['b'] <= 0]:
+                self.pc = self.instruction['c']
+            else:
+                self.pc += 3
+
+    def run(self):
+        while self.pc <= self.size:
+            self.execute()
+
+    def readMem(self, index):
+        return self.memory[index]
+
+    def readMemGrid(self, x,y):
+        # dodge as, pls fix
+        return readMem(x * sqrt(self.size) + y)
+
+    def readPC(self):
+        return self.pc
+
+    def readCurrentInstruction(self):
+        return self.instruction
+
+    def writeMem(self, value, index):
+        if self.size < index or index < 0:
+            return ValueError('Index out of range')
+        self.memory[index] = value
+
+    def loadInstructions(self, code, startPC = 0):
+        self.pc = startPC
+        for line in code:
+            m = re.match(r'subleq\s(\d+)\s(\d+)\s(\d+)',line)
+            if m is None:
+                raise ValueError('Invalid Command')
+            a = int(m.group(1))
+            b = int(m.group(2))
+            c = int(m.group(3))
+
+            self.memory[self.pc] = a
+            self.memory[self.pc+1] = b
+            self.memory[self.pc+2] = c
+            self.pc += 3
+
+if __name__ == "__main__":
+    # do tests here
+    print("test")
---
+++ main.py
@@ -0,0 +1,39 @@
+from flask import Flask, render_template, request
+
+from Machine import Machine
+
+from challengelist import challenges
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'f9843yhfiujkhsd837y4rcu43jfhsdufdsfsfj92348r this is fake mate'
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/<int:challenge_number>")
+def challenge_page(challenge_number):
+    challenge = getChallengeTextFromNumber(challenge_number)
+
+    m = Machine(length=16,height=16)
+
+    return render_template("challenge.html", challenge = challenge, all_challenges = challenges, machine = m)
+
+@app.route("/<int:challenge_number>", methods = ['POST'])
+def submit(challenge_number):
+    challenge = getChallengeTextFromNumber(challenge_number)
+
+    #if request.method == 'POST':
+
+    return render_template("challenge.html", challenge = challenge, all_challenges = challenges, submission=None)
+
+
+def getChallengeTextFromNumber(number):
+    for c in challenges:
+        if c['id'] == number:
+            return c
+    return None
+
+if __name__ == "__main__":
+    app.run()
